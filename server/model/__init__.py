5163aae78b30ed60f9503447a8788a709c7cbee4
heunyam <tpwns072@gmail.com>
2020-09-28 23:51:10 +0900

    db 연결

---
+++ server/model/__init__.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from server import DATABASE_URL
+
+engine = create_engine(DATABASE_URL)
+Base = declarative_base()
+
+Session = sessionmaker(bind=engine)
+session = Session()

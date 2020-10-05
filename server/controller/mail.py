3fdeda0a65c9c2c19e5d3f44b99d7f84e10829d3
heunyam <tpwns072@gmail.com>
2020-10-05 23:33:20 +0900

    [fix] redis connect

--- server/controller/mail.py
+++ server/controller/mail.py
@@ -1,14 +1,16 @@
 import smtplib, secrets
 from flask import abort
 from email.mime.text import MIMEText
+from datetime import timedelta
+from time import sleep
+from redis.exceptions import ConnectionError
 
 from server import EMAIL_PASSWORD, EMAIL_ID
 from server.model import Redis
 
 
-def save_code_into_redis():
-    pass
-
+def save_code_into_redis(email, auth_code):
+    Redis.set(email, auth_code, timedelta(minutes=5))
 
 def create_auth_code():
     code = secrets.choice(range(100000, 1000000))
@@ -17,13 +19,21 @@ def create_auth_code():
 
 
 def send_email(email):
+    auth_code = create_auth_code()
+
+    try:
+        save_code_into_redis(email, auth_code)
+
+    except ConnectionError:
+        abort(500, "redis_error")
+
     server = smtplib.SMTP("smtp.gmail.com", 587)
-    # 위 코드에서 에러남
     server.starttls()
     server.login(EMAIL_ID, EMAIL_PASSWORD)
 
-    msg = MIMEText(f"{create_auth_code()}")
+    msg = MIMEText(str(auth_code))
     msg['Subject'] = "yally 인증 코드"
 
     server.sendmail(EMAIL_ID, email, msg.as_string())
     server.quit()
+
--- server/model/__init__.py
+++ server/model/__init__.py
@@ -1,15 +1,13 @@
 import redis
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from server import DATABASE_URL
-
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-Redis = redis.Redis(host='localhost', port=6379, db=0)
+Redis = redis.StrictRedis(host='localhost', port=6379, db=0)

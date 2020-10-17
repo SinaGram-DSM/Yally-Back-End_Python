308cba1a9287767424a182d44529dad537b2bbd3
heunyam <tpwns072@gmail.com>
2020-10-18 03:38:51 +0900

    [feat] reset code api

--- server/controller/mail.py
+++ server/controller/mail.py
@@ -8,8 +8,8 @@ from server import EMAIL_PASSWORD, EMAIL_ID
 from server.model import Redis
 
 
-def save_code_into_redis(email, auth_code):
-    Redis.set(email, auth_code, timedelta(minutes=5))
+def save_code_into_redis(email, auth_code, codetype):
+    Redis.set(email+codetype, auth_code, timedelta(minutes=5))
 
 
 def create_auth_code():
@@ -18,11 +18,11 @@ def create_auth_code():
     return code
 
 
-def send_email(email):
-    auth_code = create_auth_code()
+def send_code_to_email(email, codetype):
+    code = create_auth_code()
 
     try:
-        save_code_into_redis(email, auth_code)
+        save_code_into_redis(email, code, codetype=codetype)
 
     except ConnectionError:
         abort(500, "redis_error")
@@ -31,9 +31,40 @@ def send_email(email):
     server.starttls()
     server.login(EMAIL_ID, EMAIL_PASSWORD)
 
-    msg = MIMEText(str(auth_code))
-    msg['Subject'] = "yally auth code"
+    msg = MIMEText(str(code))
+    msg['Subject'] = f"yally {codetype} code"
 
     server.sendmail(EMAIL_ID, email, msg.as_string())
     server.quit()
+
+    return
+
+
+
+def send_auth_code_email(email):
+    auth_code = create_auth_code()
+
+    send_code_to_email(email, codetype="auth")
+
+    return {
+        "message": "Successfully send auth code"
+    }
+
+
+def send_reset_code_email(email):
+    reset_code = create_auth_code()
+
+    send_code_to_email(email, codetype="reset")
+
+    return {
+        "message": "Successfully send reset code"
+    }
+
+
+def send_email(email, codetype):
+    if codetype == 'auth':
+        return send_auth_code_email(email)
+    elif codetype == 'reset':
+        return send_reset_code_email(email)
+
 

be41006002f157025f7f5dcaee1e248bcfb133e1
heunyam <tpwns072@gmail.com>
2020-10-18 23:01:27 +0900

    [feat] check reset(auth) code + change password

--- server/controller/mail.py
+++ server/controller/mail.py
@@ -3,9 +3,11 @@ from flask import abort
 from email.mime.text import MIMEText
 from datetime import timedelta
 from redis.exceptions import ConnectionError
+from werkzeug.security import generate_password_hash
 
 from server import EMAIL_PASSWORD, EMAIL_ID
-from server.model import Redis
+from server.model import Redis, session
+from server.model.user import User
 
 
 def save_code_into_redis(email, auth_code, codetype):
@@ -40,7 +42,6 @@ def send_code_to_email(email, codetype):
     return
 
 
-
 def send_auth_code_email(email):
     auth_code = create_auth_code()
 
@@ -68,3 +69,39 @@ def send_email(email, codetype):
         return send_reset_code_email(email)
 
 
+def check_auth_code(email, code):
+    auth_code = Redis.get(email+"auth").decode('utf-8')
+
+    if auth_code == code:
+        return {
+            "message": "Successfully authenticated"
+        }
+    else:
+        return abort(401, "The verification code is not correct")
+
+
+def check_reset_code(email, code):
+    reset_code = Redis.get(email + "reset").decode('utf-8')
+
+    if reset_code == code:
+        return True
+    else:
+        return False
+
+
+def change_password(email, code, password):
+
+    if check_reset_code(email, code):
+        user = session.query(User).filter(User.email == email).first()
+
+        if user:
+            user.password = generate_password_hash(password)
+            session.commit()
+
+            return {
+                "message": "Successfully password changed"
+            }
+        else:
+            return abort(404, "The email is incorrect")
+    else:
+        return abort(401, "The verification code is not correct")

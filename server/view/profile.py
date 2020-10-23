6980b6fd8c469f8a810117bd16c53066af8d18f2
heunyam <tpwns072@gmail.com>
2020-10-23 23:10:17 +0900

    [feat] get profile 타임라인 api

---
+++ server/view/profile.py
@@ -0,0 +1,18 @@
+from flask_restful import Resource
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from flask import abort
+from server.controller.profile import get_profile_timeline
+
+
+class GetProfileTimeline(Resource):
+
+    @jwt_required
+    def get(self, email, page):
+        user_email = get_jwt_identity()
+
+        if page.isdigit() != True:
+            return abort(400, "Check you json key name and value type")
+
+        return get_profile_timeline(user_email, email, int(page))
+
+

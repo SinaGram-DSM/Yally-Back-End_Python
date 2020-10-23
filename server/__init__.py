f1a8695f21ccaf9cd4c87eb93403ca57d6332ac6
heunyam <tpwns072@gmail.com>
2020-10-23 23:13:17 +0900

    [feat] S3관련 변수 가져오기

--- server/__init__.py
+++ server/__init__.py
@@ -3,3 +3,5 @@ from server.config import DATABASE_URL
 from server.config import JWT_SECRET_KEY
 from server.config import JWT_ACCESS_TOKEN_EXPIRES
 from server.config import EMAIL_ID, EMAIL_PASSWORD
+
+from server.config import S3_NAME, S3_REGION, S3_URL, S3_ACCESS, S3_SECRET

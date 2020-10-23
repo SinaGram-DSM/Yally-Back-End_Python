564b806818e8a22fbdd8cc40a5218d39be220ef6
heunyam <tpwns072@gmail.com>
2020-10-23 17:10:32 +0900

    [fix] yally mapping 수정

--- server/model/yally.py
+++ server/model/yally.py
@@ -5,7 +5,7 @@ from server.model import Base
 
 class Yally(Base):
     __tablename__ = 'yallies'
-    _table_args_ = {'mysql_collate': 'utf8_general_ci'}
+    __table_args__ = {'useexisting': True}
 
-    userEmail = Column(ForeignKey('users.email', onupdate="cascade", ondelete="cascade"), nullable=False)
-    postId = Column(ForeignKey('posts.id', onupdate="cascade", ondelete="cascade"), nullable=False)
+    userEmail = Column(ForeignKey('users.email', onupdate="cascade", ondelete="cascade"), primary_key=True)
+    postId = Column(ForeignKey('posts.id', onupdate="cascade", ondelete="cascade"), primary_key=True)

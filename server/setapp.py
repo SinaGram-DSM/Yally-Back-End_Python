4e184ec520c834e4357b5523bd7cb6f05a000d29
heunyam <tpwns072@gmail.com>
2020-10-24 21:21:09 +0900

    [feat] access token 없을 때 에러 핸들림 추가

--- server/setapp.py
+++ server/setapp.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, current_app
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
@@ -7,15 +7,25 @@ from server import JWT_SECRET_KEY, JWT_ACCESS_TOKEN_EXPIRES
 
 
 def create_app():
-    _app = Flask(__name__)
+    _app = Flask(__name__, instance_relative_config=True)
 
     CORS(_app)
 
-    api = Api(_app)
+    class CustomApi(Api):
+        def handle_error(self, e):
+            for val in current_app.error_handler_spec.values():
+                for handler in val.values():
+                    registered_error_handlers = list(filter(lambda x: isinstance(e, x), handler.keys()))
+                    if len(registered_error_handlers) > 0:
+                        raise e
+            return super().handle_error(e)
+
+    api = CustomApi(_app)
 
     _app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
     _app.config['JWT_ACCESS_TOKEN_EXPIRES'] = JWT_ACCESS_TOKEN_EXPIRES
-    jwt = JWTManager(_app)
+
+    JWTManager(_app)
 
     from server.view.ping import Ping
     api.add_resource(Ping, "/ping")

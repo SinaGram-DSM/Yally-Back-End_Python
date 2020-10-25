b5fa87fe3204d645571cfc72a0e8525f00a5df9e
heunyam <tpwns072@gmail.com>
2020-10-25 16:42:16 +0900

    [feat + style] return 해줄 요소 추가 + for문 한 줄로 만들어서 간단하게 만듬

--- server/controller/profile.py
+++ server/controller/profile.py
@@ -6,7 +6,6 @@ from server.model.user import User
 from server.model.post import Post
 from server.model.yally import Yally
 from server.model.comment import Comment
-from server import S3_URL
 
 
 # 무한 스크롤시 중복 post 처리는 복잡해서 아직 안함
@@ -24,28 +23,27 @@ def get_profile_timeline(user_email, profile_user_email, page):
         return abort(404, "User not Found")
 
     try:
-        post = session.query(Post) \
+        posts = session.query(Post) \
                    .filter(Post.userEmail == profile_user_email) \
                    .order_by(Post.createdAt.desc()) \
                    .all()[page:page + limit]
     except SQLAlchemyError:
         return abort(418, "db_error")
 
-    posts = []
-    if post:
-        for i in range(len(post)):
-            try:
-                posts.append({
-                    "content": post[i].content,
-                    "sound": f"{S3_URL}{post[i].sound}",
-                    "yallyCount": session.query(Yally).filter(Yally.postId == post[i].id).count(),
-                    "commentCount": session.query(Comment).filter(Comment.postId == post[i].id).count(),
-                    "isYally": True if session.query(Yally).filter(Yally.userEmail == user_email).filter(Yally.postId == post[i].id).first() else False,
-                    "createdAt": str(post[i].createdAt)
-                })
-            except SQLAlchemyError:
-                return abort(418, "db_error")
-
     return {
-        "posts": posts
+        "posts": [{
+            "id": post.id,
+            "user": {
+                "email": user.email,
+                "nickname": user.nickname,
+                "img": user.img
+            },
+            "content": post.content,
+            "sound": post.sound,
+            "img": post.img,
+            "yally": session.query(Yally).filter(Yally.postId == post.id).count(),
+            "comment": session.query(Comment).filter(Comment.postId == post.id).count(),
+            "isYally": True if session.query(Yally).filter(Yally.userEmail == user_email).filter(Yally.postId == post[i].id).first() else False,
+            "createdAt": str(post.createdAt)
+        }for post in posts]
     }

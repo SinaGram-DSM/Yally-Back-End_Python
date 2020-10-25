0826de3c9b23c0e2f4af8cc305ac8ee339b53ed3
heunyam <tpwns072@gmail.com>
2020-10-25 11:04:31 +0900

    [feat] user nickname search api

---
+++ server/controller/search.py
@@ -0,0 +1,24 @@
+from sqlalchemy.exc import SQLAlchemyError
+from flask import abort
+
+from server.model import session
+from server.model.user import User
+from server.model.listen import Listen
+from server import S3_URL
+
+
+def get_user_search_info(owner_email, user_nickname):
+    try:
+        user = session.query(User).filter(User.nickname.like(f"{user_nickname}%")).all()
+    except SQLAlchemyError:
+        return abort(418, "db_error")
+
+    return {
+        "users": [{
+            "img": f"{S3_URL}{user_info.img}",
+            "nickname": user_info.nickname,
+            "listening": session.query(Listen).filter(Listen.listeningEmail == user_info.email).count(),
+            "listener": session.query(Listen).filter(Listen.listenerEmail == user_info.email).count(),
+            "isListening": True if session.query(Listen).filter(Listen.listenerEmail == owner_email).filter(Listen.listeningEmail == user_info.email).first() else False
+        }for user_info in user]
+    }

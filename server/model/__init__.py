0dfb24c6b9a5926bb38bda737ef885ac0e8852c9
heunyam <tpwns072@gmail.com>
2020-10-30 22:49:53 +0900

    [feat] engine에 pool 관련 옵션 추가

--- server/model/__init__.py
+++ server/model/__init__.py
@@ -4,7 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from server import DATABASE_URL
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, encoding="utf-8", pool_size=20,
+                       pool_recycle=500, max_overflow=20)
+
 Base = declarative_base()
 
 Session = sessionmaker(bind=engine)

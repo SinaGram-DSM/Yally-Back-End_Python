aa9184b0802d133d1b38fa554ec50ff5885c72af
Ubuntu <ubuntu@ip-172-31-2-237.ap-northeast-2.compute.internal>
2020-11-07 13:39:41 +0000

    [fix] session config

--- server/model/__init__.py
+++ server/model/__init__.py
@@ -1,15 +1,14 @@
 import redis
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 from server import DATABASE_URL
 engine = create_engine(DATABASE_URL, encoding="utf-8", pool_size=20,
-                       pool_recycle=500, max_overflow=20, pool_pre_ping=True)
+                       pool_recycle=3600, max_overflow=20, pool_pre_ping=True)
 
 Base = declarative_base()
 
-Session = sessionmaker(bind=engine)
-session = Session()
+session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Redis = redis.StrictRedis(host='localhost', port=6379, db=0)
